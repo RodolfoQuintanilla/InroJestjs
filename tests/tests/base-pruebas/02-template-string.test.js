671038eb6508548c916e8705e31c7509ea0f50ec
Rodolfo <rqm@outlook.es>
2024-11-12 15:58:54 -0600

    pruebas v-56

---
+++ tests/tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,10 @@
+import { getSaludo } from '../../src/base-pruebas/02-template-string';
+
+describe('Pruebas en 02-template', () => {
+    test('getSaludo debe retornar hola fernando', () => {
+        const name = 'fernando';
+        const message = getSaludo(name);
+
+        expect(message).toBe(`Hola ${name}!!!`)
+    })
+});
--- tests/tests/demo.test.js
+++ tests/tests/demo.test.js
@@ -1,8 +1,17 @@
-test('some test', () => {
 
-    //1. Inicializacion
+describe('pruebas de <Demo/>', () => {
 
-    //2. estimulo
 
-    //3. Observar el comportamiento... esperado
+
+    test('some test', () => {
+
+        //1. Inicializacion
+        const mesj = 'Hola Pero'
+        //2. estimulo
+        const mesj2 = mesj.trim();
+        //3. Observar el comportamiento... 
+        expect(mesj2).toBe('Hola Pero')
+        //4. Validacion
+        //console.log(mesj2);#include <stdio.h>
+    })
 })
