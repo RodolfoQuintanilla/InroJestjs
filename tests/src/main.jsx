acdbf62cc289c39c5592244d989e82a5c9c0fcd8
Rodolfo <rqm@outlook.es>
2024-11-16 12:30:51 -0600

    proyecto pasadp

--- tests/src/main.jsx
+++ tests/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import FirstApp from './FirstApp.jsx'
+import { FirstApp } from './FirstApp'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FirstApp />
+    <FirstApp  title="Hola, soy Goku" />
   </StrictMode>,
 )
